0c87059410507f31ad0152e0fed706efc2f7f4e5
Wallace Gleen <wallace.gleen@gmail.com>
2020-12-10 02:21:50 -0300

    change app test to use enzyme

--- src/components/core/App/app.spec.js
+++ src/components/core/App/app.spec.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { mount } from 'enzyme'
 import App from './App'
 
 describe('<App />', () => {
   it('should render correctly', () => {
-    const component = render(<App />)
+    const component = mount(<App />)
 
     expect(component).toMatchSnapshot()
 

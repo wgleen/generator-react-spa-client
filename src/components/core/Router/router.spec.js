40af544d7029c1976f714f330cb7c073151651c7
Wallace Gleen <wallace.gleen@gmail.com>
2020-12-10 02:22:50 -0300

    change tests of router to enzyme

--- src/components/core/Router/router.spec.js
+++ src/components/core/Router/router.spec.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { mount } from 'enzyme'
 import Router from './Router'
 
 describe('<Router />', () => {
   it('should render correctly', () => {
-    const component = render(<Router />)
+    const component = mount(<Router />)
 
     expect(component).toMatchSnapshot()
 
-    screen.debug(component.container)
+    component.unmount()
+  })
+
+  it('should render the RouterProvider component containing the Routes component', () => {
+    const component = mount(<Router />)
+
+    expect(component.find('RouterProvider').find('Routes').exists()).toBeTrue()
 
     component.unmount()
   })

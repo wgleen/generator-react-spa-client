c84775e56e89ab163ddb3c86bad722e8f9d9d131
Wallace Gleen <wallace.gleen@gmail.com>
2020-12-10 00:12:48 -0300

    add Router component with RouterProvider and Routes

---
+++ src/components/core/Router/Router.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import RouterProvider from './RouterProvider'
+import Routes from './Routes'
+
+const Router = () => (
+  <RouterProvider>
+    <Routes />
+  </RouterProvider>
+)
+
+export default Router
